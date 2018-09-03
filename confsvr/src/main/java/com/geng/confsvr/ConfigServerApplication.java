176735c1ff3dd741cac551f2c253d1bdeae4b806
gengbushuang <gengbushuang@apusapps.com>
2018-09-03 18:14:49 +0800

    confsvr云配置文件服务

--- confsvr/src/main/java/com/geng/confsvr/ConfigServerApplication.java
+++ confsvr/src/main/java/com/geng/confsvr/ConfigServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 //使服务成为Sping Cloud Conifg服务
 @EnableConfigServer
-public class Application {
+@SpringBootApplication
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
